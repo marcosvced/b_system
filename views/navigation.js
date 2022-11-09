8a316ab95653c4dea6cd543c54bbb20f6c62aa2d
Marcos Vicente Espasandín <marcos.vicente@storybeat.com>
2022-11-09 13:45:11 +0100

    chore: desktop super-menu

--- views/navigation.js
+++ views/navigation.js
@@ -25,155 +25,206 @@ const navigation = [
     key: 'modules',
     entries: [
       {
-        href: QUICK_START.CATEGORIES_MENU,
-        label: 'Menus',
+        group: [
+          {
+            label: 'Basics',
+            entries: [
+              {
+                href: QUICK_START.HEADERS,
+                label: 'Headers',
+              },
+              {
+                href: QUICK_START.BASIC_CONTENTS,
+                label: 'Basic Contents',
+              },
+              {
+                href: QUICK_START.CTA,
+                label: 'CTA',
+              },
+              {
+                href: QUICK_START.SEARCHERS,
+                label: 'Searchers',
+              },
+              {
+                href: QUICK_START.CATEGORIES_MENU,
+                label: 'Menus',
+              },
+              {
+                href: QUICK_START.ICONS_SOCIAL,
+                label: 'Icons Social',
+              },
+              {
+                href: QUICK_START.ICON_DESCRIPTION,
+                label: 'Icon + description'
+              },
+              {
+                href: QUICK_START.ICON_SERVICES,
+                label: 'Icon Services',
+              },
+              {
+                href: QUICK_START.STATISTICS,
+                label: 'Statistics',
+              },
+              {
+                href: QUICK_START.FAQS,
+                label: 'FAQs',
+              },
+              {
+                href: QUICK_START.CONTACT_BAR,
+                label: 'Contact Bar',
+              },
+            ]
+          },
+        ]
       },
       {
-        href: QUICK_START.HEADERS,
-        label: 'Headers',
+        group: [
+          {
+            label: 'Marquees',
+            entries: [
+              {
+                href: QUICK_START.MARQUEES.BASIC,
+                label: 'Basic',
+              },
+              {
+                href: QUICK_START.MARQUEES.STANDAR,
+                label: 'Standard',
+              },
+              {
+                href: QUICK_START.MARQUEES.CTA,
+                label: 'With CTA',
+              },
+              {
+                href: QUICK_START.MARQUEES.IMAGE,
+                label: 'Image',
+              },
+              {
+                href: QUICK_START.MARQUEES.TEXTIMAGE,
+                label: 'Text and image',
+              },
+              {
+                href: QUICK_START.MARQUEES.ILLUSTRATION,
+                label: 'Illustration',
+              },
+              {
+                href: QUICK_START.MARQUEES.EXTENDED,
+                label: 'Extended',
+              },
+            ]
+          },
+          {
+            label: 'Cards',
+            entries: [
+              {
+                href: QUICK_START.FEATURED_CARDS,
+                label: 'Featured',
+              },
+              {
+                href: QUICK_START.PRODUCT_GRIDS,
+                label: 'Product',
+              },
+            ]
+          },
+        ]
       },
       {
-        href: QUICK_START.SERVICE_GRIDS,
-        label: 'Service grid',
+        group: [
+          {
+            label: 'Grids',
+            entries: [
+              {
+                href: QUICK_START.BASIC_GRID,
+                label: 'Basic',
+              },
+              {
+                href: QUICK_START.SERVICE_GRIDS,
+                label: 'Service',
+              },
+              {
+                href: QUICK_START.TEAM_GRIDS,
+                label: 'Team',
+              },
+            ]
+          },
+          {
+            label: 'Carrousels',
+            entries: [
+              {
+                href: QUICK_START.BUSINESS_CASES,
+                label: 'Business Cases',
+              },
+              {
+                href: QUICK_START.SERVICES,
+                label: 'Service',
+              },
+              {
+                href: QUICK_START.BRANDS,
+                label: 'Brands',
+              },
+              {
+                href: QUICK_START.TESTIMONIALS,
+                label: 'Testimonials',
+              },
+              {
+                href: QUICK_START.PRICES,
+                label: 'Prices Carousel',
+              },
+            ]
+          },
+        ]
       },
       {
-        href: QUICK_START.TEAM_GRIDS,
-        label: 'Team grids',
-      },
-      {
-        href: QUICK_START.ICONS_SOCIAL,
-        label: 'Icons Social',
-      },
-      {
-        href: '#',
-        label: 'Forms',
-      },
-      {
-        href: QUICK_START.ICON_DESCRIPTION,
-        label: 'Icon + description'
-      },
-      {
-        href: QUICK_START.FEATURED_CARDS,
-        label: 'Featured Cards',
-      },
-      {
-        href: QUICK_START.FAQS,
-        label: 'FAQs',
-      },
-      {
-        href: QUICK_START.ICON_SERVICES,
-        label: 'Icon Services',
-      },
-      {
-        href: QUICK_START.STATISTICS,
-        label: 'Statistics',
-      },
-      {
-        href: QUICK_START.BUSINESS_CASES,
-        label: 'Business Cases',
-      },
-      {
-        href: QUICK_START.CONTACTS,
-        label: 'Contact',
-      },
-      {
-        href: QUICK_START.SERVICES,
-        label: 'Service',
-      },
-      {
-        href: QUICK_START.PORTFOLIOS.BASIC,
-        label: 'Portfolio Basic',
-      },
-      {
-        href: QUICK_START.PORTFOLIOS.DECOMPOSED,
-        label: 'Portfolio Decomposed',
-      },
-      {
-        href: QUICK_START.PRICES,
-        label: 'Prices Carousel',
-      },
-      {
-        href: QUICK_START.FOOTERS.BASIC,
-        label: 'Footers',
-      },
-      {
-        href: QUICK_START.FOOTERS.STANDARD,
-        label: 'Footers Standard',
-      },
-      {
-        href: QUICK_START.FOOTERS.CORPORATE,
-        label: 'Footers Corporate',
-      },
-      {
-        href: QUICK_START.FOOTERS.NEWSLETTER,
-        label: 'Footers Newsletter',
-      },
-      {
-        href: QUICK_START.CTA,
-        label: 'CTA',
-      },
-      {
-        href: QUICK_START.LOGINS,
-        label: 'Login',
-      },
-      {
-        href: QUICK_START.BRANDS,
-        label: 'Brands',
-      },
-      {
-        href: QUICK_START.BASIC_GRID,
-        label: 'Basic Grid',
-      },
-      {
-        href: QUICK_START.TESTIMONIALS,
-        label: 'Testimonials',
-      },
-      {
-        href: QUICK_START.PRODUCT_GRIDS,
-        label: 'Product Cards',
-      },
-      {
-        href: QUICK_START.SEARCHERS,
-        label: 'Searchers',
-      },
-      {
-        href: QUICK_START.CONTACT_BAR,
-        label: 'Contact Bar',
-      },
-      {
-        href: QUICK_START.BASIC_CONTENTS,
-        label: 'Basic Contents',
-      },
-      {
-        href: QUICK_START.MARQUEES.BASIC,
-        label: 'Marquee',
-      },
-      {
-        href: QUICK_START.MARQUEES.CTA,
-        label: 'Marquee cta',
-      },
-      {
-        href: QUICK_START.MARQUEES.EXTENDED,
-        label: 'Marquee extended',
-      },
-      {
-        href: QUICK_START.MARQUEES.ILLUSTRATION,
-        label: 'Marquee illustration',
-      },
-      {
-        href: QUICK_START.MARQUEES.IMAGE,
-        label: 'Marquee image',
-      },
-      {
-        href: QUICK_START.MARQUEES.STANDAR,
-        label: 'Marquee standar',
-      },
-      {
-        href: QUICK_START.MARQUEES.TEXTIMAGE,
-        label: 'Marquee textimage',
-      },
+        group: [
+          {
+            label: 'Portfolio',
+            entries: [
+              {
+                href: QUICK_START.PORTFOLIOS.BASIC,
+                label: 'Portfolio Basic',
+              },
+              {
+                href: QUICK_START.PORTFOLIOS.DECOMPOSED,
+                label: 'Portfolio Decomposed',
+              },
+            ]
+          },
+          {
+            label: 'Forms',
+            entries: [
+              {
+                href: QUICK_START.LOGINS,
+                label: 'Login',
+              },
+              {
+                href: QUICK_START.CONTACTS,
+                label: 'Contact',
+              },
+            ]
+          },
+          {
+            label: 'Footers',
+            entries: [
+              {
+                href: QUICK_START.FOOTERS.BASIC,
+                label: 'Basic',
+              },
+              {
+                href: QUICK_START.FOOTERS.STANDARD,
+                label: 'Standard',
+              },
+              {
+                href: QUICK_START.FOOTERS.CORPORATE,
+                label: 'Corporate',
+              },
+              {
+                href: QUICK_START.FOOTERS.NEWSLETTER,
+                label: 'Newsletter',
+              },
+            ]
+          },
+        ]
+      }
     ],
-
   },
   {
     label: 'Components',
