be2b522c4800538dc1e252b4cb93002bfb02fd06
Marcos Vicente Espasandín <marcos.vicente@storybeat.com>
2023-03-19 22:37:25 +0100

    feat: test code preview

--- router/routeBuild.js
+++ router/routeBuild.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const beautify = require('js-beautify').js;
+const beautify = require('js-beautify').html;
 const pretty = require('pretty');
 
 const distPath = `${process.cwd()}/dist`;
@@ -10,42 +10,36 @@ module.exports = (response, path, html) => {
     fs.mkdirSync(`${distPath}${path}/`, { recursive: true });
   }
 
-  const dirDepth = path.split('/')
-    .map((value) => {
-      return value ? '..' : '';
-    })
-    .filter((n) => n)
-    .join('/');
-
   const options = {
-    indent_size: '4',
+    indent_size: '2',
     indent_char: ' ',
     max_preserve_newlines: '5',
-    preserve_newlines: false,
+    preserve_newlines: true,
     keep_array_indentation: false,
     break_chained_methods: false,
-    indent_scripts: 'normal',
+    indent_scripts: 'separate',
     brace_style: 'collapse',
     space_before_conditional: true,
     unescape_strings: false,
-    jslint_happy: true,
-    end_with_newline: true,
-    wrap_line_length: '120',
-    indent_inner_html: false,
+    jslint_happy: false,
+    end_with_newline: false,
+    wrap_line_length: '0',
+    indent_inner_html: true,
     comma_first: false,
     e4x: false,
     indent_empty_lines: false,
   };
 
-  let replacedHtml = beautify(html, options);
-  replacedHtml = replacedHtml.replace(/\/assets/g, `${dirDepth}/assets`)
+  const dirDepth = path.split('/')
+    .map((value) => (value ? '..' : ''))
+    .filter((n) => n)
+    .join('/');
+  const replacedHtml = html.replace(/\/assets/g, `${dirDepth}/assets`)
     .replace(/href="\/\//g, 'href="cdn//')
     .replace(/href="\//g, `href="${dirDepth}/`)
-    .replace(/href="cdn/g, 'href="')
-    .replace(/ {2,}/g, ' ');
+    .replace(/href="\//g, `href="${dirDepth}/`)
+    .replace(/href="cdn/g, 'href="');
 
-
-  fs.writeFileSync(`${distPath}${path}/index.html`, replacedHtml);
-
+  fs.writeFileSync(`${distPath}${path}/index.html`, beautify(replacedHtml, options));
   response.send(html);
 };
